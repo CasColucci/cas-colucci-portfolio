9c02e621bc1073673ffaa4576499050406cfc79c
CasColucci <crpadgitt@gmail.com>
2026-06-04 15:10:23 -0400

    Trying the alternate netlify attribute

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -212,7 +212,7 @@
         <SectionHead number="05" title="Contact" />
         <hr>
         <div class="contact">
-            <form class="contact-form" method="POST" netlify name="contact">
+            <form class="contact-form" method="POST" data-netlify="true" name="contact">
                 <label for="name">Name</label>
                 <input type="text" id="name" name="name" placeholder="Name" required>
 
